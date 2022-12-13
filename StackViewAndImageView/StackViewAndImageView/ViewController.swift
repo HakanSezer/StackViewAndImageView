a36a1d23ce85ec2e41796bfe4ba1437882aa192b
Hakan Sezer <62359186+HakanSezer@users.noreply.github.com>
2022-12-13 09:30:32 +0300

    StackView&ImageView
    StackView ve Image View üzerine yapmış olduğum çalışmam. AutoLayout'ta yapılmıştır.

---
+++ StackViewAndImageView/StackViewAndImageView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StackViewAndImageView
+//
+//  Created by Hakan Sezer on 12.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var randomImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func goRandomButton (_ sender: UIButton) {
+        
+    }
+
+}
+
